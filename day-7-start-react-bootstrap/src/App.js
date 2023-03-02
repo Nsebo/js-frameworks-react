4d90ff88924ee5db615a318191aa38070297967b
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-03-02 15:31:16 +0100

    dat 7 completes

--- day-7-start-react-bootstrap/src/App.js
+++ day-7-start-react-bootstrap/src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import Nav from 'react-bootstrap/Nav';
+
 
 function App() {
   return (
     <div className="App">
+        <Nav
+            activeKey="/home"
+            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
     </div>
   );
 }
