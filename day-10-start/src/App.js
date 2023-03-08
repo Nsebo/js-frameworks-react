c72f0ab63b8f5dd76b04f298467c3d44d8d43810
Uzochukwu Cecilia Oloh <Fruklem16@gmail.com>
2023-03-08 14:18:46 +0100

    useEffect first and second examples

---
+++ day-10-start/src/App.js
@@ -0,0 +1,15 @@
+// import FirstExample from './components/FirstExample';
+import SecondExample from './components/SecondExample';
+
+function App() {
+  return (
+    <div className="App">
+      {/* FirstExample */}
+      {/*<ExampleOne/>*/}
+
+      <SecondExample />
+    </div>
+  );
+}
+
+export default App;
